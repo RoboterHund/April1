db465ffe2854df67f52552e15e7e52e7f63a00de
Alexander Diaz Chub <roboterhund@gmail.com>
2014-06-27 00:27:40 +0200

    YOU WILL NOT IGNORE MY GITIGNORE, GIT

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 'use strict';
 
 module.exports = {
-	consumer: require ('./core-modules/consumer'),
-	defaults: require ('./core-modules/defaults'),
-	output: require ('./core-modules/output'),
-	params: require ('./core-modules/params'),
-	spec: require ('./core-modules/spec'),
-	template: require ('./core-modules/template'),
-	types: require ('./core-modules/types')
+	consumer: require ('./core-modulez/consumer'),
+	defaults: require ('./core-modulez/defaults'),
+	output: require ('./core-modulez/output'),
+	params: require ('./core-modulez/params'),
+	spec: require ('./core-modulez/spec'),
+	template: require ('./core-modulez/template'),
+	types: require ('./core-modulez/types')
 };
